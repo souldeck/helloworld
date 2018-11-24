c4e632562463feea0b94a0c2e6f1b91b9dd2bf4e
이경하 <@192.168.0.35>
2018-11-25 06:15:08 +0900

    연습

--- src/helloworld.java
+++ src/helloworld.java
@@ -1,6 +1,6 @@
 
 public class helloworld {
-	public static void main(String[] args) {
+	public static void main(Strings[]args) {
 		System.out.println("Hello World");
 		System.out.println("I'm new");
 	}
